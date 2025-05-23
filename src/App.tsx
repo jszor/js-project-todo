15a4ea4c0bda6c8ccb3cb8753427f167288cd065
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-05-23 17:11:43 +0200

    add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 export const App = () => {
   return (
-    <h1>To-Do List (Neobrutalism Edition)</h1>
+    <>
+    <h1 className="font-heading text-3xl">To-Do List (Neobrutalism Edition)</h1>
+    <div className="bg-testblue text-blue-800">Is this blue?</div>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
+      colors: {
+        testblue: "#00f",
+      },
       fontFamily: {
-        heading: ['Lexend Mega', 'sans-serif'],
-        base: ['Lexend Mega', 'sans-serif'],
-      }
+        heading: ['"Lexend Mega"', 'sans-serif'],
+      },
     },
   },
-}
+  plugins: [],
+};
