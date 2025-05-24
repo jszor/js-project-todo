4ffc7a7375fc7d4821f52e044f7f3d3ae289ebdb
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-05-24 23:07:48 +0200

    create todo store

---
+++ src/stores/useTodoStore.ts
@@ -0,0 +1,32 @@
+import { create } from 'zustand'
+
+type Todo = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
+type TodoState = {
+  todos: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
+};
+
+const useTodoStore = create<TodoState>((set) => ({
+  todos: [],
+  addTodo: (text) => 
+    set((state) => ({
+      todos: [...state.todos, {id: Date.now(), text, completed: false}],
+    })),
+  removeTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.filter((todo) => todo.id !== id),
+    })),
+  toggleTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
+    })),
+}))
