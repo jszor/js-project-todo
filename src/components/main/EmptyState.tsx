c99114518e428cd3b6d9d7215e2c5945906515f6
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-05-26 11:22:03 +0200

    change empty state colors for better a11y

--- src/components/main/EmptyState.tsx
+++ src/components/main/EmptyState.tsx
@@ -1,7 +1,7 @@
 const EmptyState = () => {
   return (
-    <div className="bg-background w-5/6 mx-auto border-2 border-slate-500 border-dashed rounded-[5px] flex flex-col items-center justify-center min-h-[180px]">
-      <p className="text-sm text-slate-500 text-center p-5">Hooray, no active tasks!</p>
+    <div className="bg-background w-5/6 mx-auto border-2 border-slate-600 border-dashed rounded-[5px] flex flex-col items-center justify-center min-h-[180px]">
+      <p className="text-sm text-slate-600 text-center p-5">Hooray, no active tasks!</p>
     </div>
   )
 }
